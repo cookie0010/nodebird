9ba1ff5544fc1d5132c0ac4842b5008e7f7592f3
cookie0010 <sg.been2017@gmail.com>
2022-12-14 20:35:39 +0900

    jwt 마무리

--- nodecat/routes/index.js
+++ nodecat/routes/index.js
@@ -2,33 +2,53 @@ const express = require("express");
 const axios = require("axios");
 
 const router = express.Router();
+const URL = "http://localhost:8002/v1";
 
-router.get("/test", async (req, res, next) => {
+axios.defaults.headers.origin = "http://localhost:4000";
+const request = async (req, api) => {
 	try {
 		if (!req.session.jwt) {
-			const tokenResult = await axios.post("http://localhost:8002/v1/token", {
+			// 세션에 토큰이 없으면 토큰 발급 시도
+			const tokenResult = await axios.post(`${URL}/token`, {
 				clientSecret: process.env.CLIENT_SECRET,
 			});
-			if (tokenResult.data && tokenResult.data.code === 200) {
-				// 토큰 발급 성공
-				req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
-			} else {
-				return res.json(tokenResult.data); // 발급 실패 사유 응답
-			}
+			req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
 		}
-		// 발급받은 토큰 테스트
-		const result = await axios.get("http://localhost:8002/v1/test", {
+		// req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
+		return await axios.get(`${URL}${api}`, {
 			headers: { authorization: req.session.jwt },
 		});
-		return res.json(result.data);
 	} catch (err) {
 		console.error(err);
 		if (err.response.status === 419) {
 			// 토큰 만료시
-			return res.json(error.response.data);
+			delete req.session.jwt; // 토큰 삭제하고
+			return request(req, api); // request 다시 실행
 		}
-		return next(err);
+		return err.response;
+	}
+};
+
+router.get("/mypost", async (req, res, next) => {
+	try {
+		const result = await request(req, "/posts/my");
+		res.json(result.data);
+	} catch (error) {
+		console.error(error);
+		next(error);
 	}
 });
 
+router.get("/search/:hashtag", async (req, res, next) => {
+	try {
+		const result = await request(
+			req,
+			`/posts/hashtag/${encudeURIComponent(req.params.hashtag)}`,
+		);
+		res.json(result.data);
+	} catch (err) {
+		console.error(err);
+		next(err);
+	}
+});
 module.exports = router;
