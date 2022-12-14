080fee3d3aa76ac82743e7107a7abb064b2b1216
cookie0010 <sg.been2017@gmail.com>
2022-12-14 17:50:58 +0900

    nodecat으로 토큰 발급받기

--- nodebird-api/routes/v1.js
+++ nodebird-api/routes/v1.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/token", async (req, res) => {
 	const { clientSecret } = req.body;
 	try {
-		const domain = await Domain.find({
+		const domain = await Domain.findOne({
 			where: { clientSecret },
 			include: {
 				model: User,
@@ -23,9 +23,10 @@ router.post("/token", async (req, res) => {
 			});
 		}
 		const token = jwt.sign(
+			// 토큰 발급
 			{
-				id: domain.user.id,
-				nick: domain.user.nick,
+				id: domain.User.id,
+				nick: domain.User.nick,
 			},
 			process.env.JWT_SECRET,
 			{
--- nodecat/app.js
+++ nodecat/app.js
@@ -1,40 +1,22 @@
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const passport = require("passport");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const nunjucks = require("nunjucks");
 const dotenv = require("dotenv");
 
-dotenv.config(); // dotenv 파일 사용. 변수를 사용하기 위해선 최대한 위쪽에 위치하는 것이 좋다.
-const authRouter = require("./routes/auth");
+dotenv.config();
 const indexRouter = require("./routes");
-const v1 = require("./routes/v1");
-const { sequelize } = require("./models");
-const passportConfig = require("./passport");
 
 const app = express();
-passportConfig();
-app.set("port", process.env.PORT || 8002);
+app.set("port", process.env.PORT || 4000);
 app.set("view engine", "html");
 nunjucks.configure("views", {
 	express: app,
 	watch: true,
 });
-sequelize
-	.sync({ force: false })
-	.then(() => {
-		console.log("데이터베이스 연결 성공");
-	})
-	.catch((err) => {
-		console.error(err);
-	});
 
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json()); // json 형식을 지원하겠다
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
 	session({
@@ -42,17 +24,13 @@ app.use(
 		saveUninitialized: false,
 		secret: process.env.COOKIE_SECRET,
 		cookie: {
-			httpOnly: true,
+			httpOnl: true,
 			secure: false,
 		},
 	}),
 );
-app.use(passport.initialize());
-app.use(passport.session());
 
-app.use("/auth", authRouter);
 app.use("/", indexRouter);
-app.use("/v1", v1);
 
 app.use((req, res, next) => {
 	const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
